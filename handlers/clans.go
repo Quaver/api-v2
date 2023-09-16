aaa38e3211f316cf3be34e5e387eff6e43bbd871
Swan <me@swan.moe>
2023-09-16 16:37:17 -0400

    Add endpoint: prefix to comment

--- handlers/clans.go
+++ handlers/clans.go
@@ -3,14 +3,13 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 // GetClans Retrieves basic info / leaderboard data about clans
-// GET /v2/clans?page=1
+// Endpoint: GET /v2/clans?page=1
 func GetClans(c *gin.Context) {
 }
 
 // CreateClan Creates a new clan if the user is eligible to.
-// POST /v2/clan
+// Endpoint: POST /v2/clan
 func CreateClan(c *gin.Context) {
-	// TODO: Authenticate user
 }
 
 // GetClan Retrieves data about an individual clan
@@ -19,11 +18,11 @@ func GetClan(c *gin.Context) {
 }
 
 // UpdateClan Updates data about a clan
-// PATCH /v2/clan/:id
+// Endpoint: PATCH /v2/clan/:id
 func UpdateClan(c *gin.Context) {
 }
 
 // DeleteClan Deletes an individual clan
-// DELETE /v2/clan/:id
+// Endpoint: DELETE /v2/clan/:id
 func DeleteClan(c *gin.Context) {
 }
