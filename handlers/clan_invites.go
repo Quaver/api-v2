efcf933b0b367da5786a91d2ec8871a037159287
Swan <me@swan.moe>
2023-09-26 12:06:48 -0400

    Refactor InviteUserToClan to new handler system

--- handlers/clan_invites.go
+++ handlers/clan_invites.go
@@ -1,61 +1,58 @@
 package handlers
 
 import (
+	"github.com/Quaver/api2/db"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
 )
 
 func InviteUserToClan(c *gin.Context) *APIError {
+	user, apiErr := authenticateUser(c)
+
+	if apiErr != nil {
+		return apiErr
+	}
+
+	if user.ClanId == nil {
+		return APIErrorForbidden("You are not currently in a clan.")
+	}
+
+	_, apiErr = getClanAndCheckOwnership(user, *user.ClanId)
+
+	if apiErr != nil {
+		return apiErr
+	}
+
+	body := struct {
+		UserId int `form:"user_id" json:"user_id" binding:"required"`
+	}{}
+
+	if err := c.ShouldBind(&body); err != nil {
+		return APIErrorBadRequest("Invalid request body")
+	}
+
+	if user.Id == body.UserId {
+		return APIErrorBadRequest("You cannot invite yourself to the clan.")
+	}
+
+	invitee, err := db.GetUserById(body.UserId)
+
+	switch err {
+	case nil:
+		break
+	case gorm.ErrRecordNotFound:
+		return APIErrorBadRequest("That user does not exist.")
+	default:
+		return APIErrorServerError("Error retrieving user in db", err)
+	}
+
+	if invitee.ClanId != nil {
+		return APIErrorBadRequest("You cannot invite that user because they are already in a clan.")
+	}
+
+	// TODO: Check for pending invitation
+	// TODO: Invite the user
+	c.JSON(http.StatusOK, "INVITED USER TO CLAN")
 	return nil
-	/*	user, apiErr := authenticateUser(c)
-
-		if apiErr != nil {
-			return apiErr
-		}
-
-		if user.ClanId == nil {
-		//	return
-		}
-
-		_, err := getClanAndCheckOwnership(c, user, *user.ClanId)
-
-		if err != nil {
-		//	return
-		}
-
-		body := struct {
-			UserId int `form:"user_id" json:"user_id" binding:"required"`
-		}{}
-
-		if err := c.ShouldBind(&body); err != nil {
-			//	Return400(c)
-		//	return
-		}
-
-		if user.Id == body.UserId {
-			ReturnError(c, http.StatusBadRequest, "You cannot invite yourself to the clan.")
-		//	return
-		}
-
-		invitee, err := db.GetUserById(body.UserId)
-
-		switch err {
-		case nil:
-			break
-		case gorm.ErrRecordNotFound:
-			ReturnError(c, http.StatusBadRequest, "That user does not exist.")
-			return
-		default:
-			logrus.Error("Error getting user in db: ", err)
-			//	Return500(c)
-			return
-		}
-
-		if invitee.ClanId != nil {
-			ReturnError(c, http.StatusBadRequest, "You cannot invite that user because they are already in a clan.")
-			return
-		}
-
-		// TODO: Check for pending invitation
-		// TODO: Invite the user
-		c.JSON(http.StatusOK, "INVITED USER TO CLAN")*/
 }
