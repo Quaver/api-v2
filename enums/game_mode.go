40addc4a58c0c5f8c4aacb4bb116c873818d9324
Swan <me@swan.moe>
2024-02-06 11:13:34 -0500

    Add function to get stringified game mode

--- enums/game_mode.go
+++ enums/game_mode.go
@@ -7,3 +7,15 @@ const (
 	GameModeKeys4
 	GameModeKeys7
 )
+
+// GetGameModeString Returns a game mode int in its stringified name
+func GetGameModeString(mode GameMode) string {
+	switch mode {
+	case GameModeKeys4:
+		return "keys4"
+	case GameModeKeys7:
+		return "keys7"
+	default:
+		return "not_implemented"
+	}
+}
