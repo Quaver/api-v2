251eee038fe36e66d1ce3c5834d5571aa4f35c73
Swan <me@swan.moe>
2024-03-25 11:09:40 -0400

    Fix .qua file parsing

--- qua/qua.go
+++ qua/qua.go
@@ -3,36 +3,50 @@ package qua
 import (
 	"github.com/Quaver/api2/enums"
 	"github.com/goccy/go-yaml"
+	"github.com/sirupsen/logrus"
 )
 
 type Qua struct {
-	AudioFile                      string         `yaml:"AudioFile"`
-	SongPreviewTime                int            `yaml:"SongPreviewTime"`
-	BackgroundFile                 string         `yaml:"BackgroundFile"`
-	BannerFile                     string         `yaml:"BannerFile"`
-	MapId                          int            `yaml:"MapId"`
-	MapSetId                       int            `yaml:"MapSetId"`
-	Mode                           enums.GameMode `yaml:"Mode"`
-	Title                          string         `yaml:"Title"`
-	Artist                         string         `yaml:"Artist"`
-	Source                         string         `yaml:"Source"`
-	Tags                           string         `yaml:"Tags"`
-	Creator                        string         `yaml:"Creator"`
-	DifficultyName                 string         `yaml:"DifficultyName"`
-	Description                    string         `yaml:"Description"`
-	Genre                          string         `yaml:"Genre"`
-	BPMDoesNotAffectScrollVelocity bool           `yaml:"BPMDoesNotAffectScrollVelocity"`
-	InitialScrollVelocity          float32        `yaml:"InitialScrollVelocity"`
-	HasScratchKey                  bool           `yaml:"HasScratchKey"`
+	AudioFile                      string `yaml:"AudioFile"`
+	SongPreviewTime                int    `yaml:"SongPreviewTime"`
+	BackgroundFile                 string `yaml:"BackgroundFile"`
+	BannerFile                     string `yaml:"BannerFile"`
+	MapId                          int    `yaml:"MapId"`
+	MapSetId                       int    `yaml:"MapSetId"`
+	RawMode                        string `yaml:"Mode"`
+	Mode                           enums.GameMode
+	Title                          string  `yaml:"Title"`
+	Artist                         string  `yaml:"Artist"`
+	Source                         string  `yaml:"Source"`
+	Tags                           string  `yaml:"Tags"`
+	Creator                        string  `yaml:"Creator"`
+	DifficultyName                 string  `yaml:"DifficultyName"`
+	Description                    string  `yaml:"Description"`
+	Genre                          string  `yaml:"Genre"`
+	BPMDoesNotAffectScrollVelocity bool    `yaml:"BPMDoesNotAffectScrollVelocity"`
+	InitialScrollVelocity          float32 `yaml:"InitialScrollVelocity"`
+	HasScratchKey                  bool    `yaml:"HasScratchKey"`
 }
 
 // Parse Parses and returns a Qua file
 func Parse(file []byte) (*Qua, error) {
-	var qua *Qua
+	qua := Qua{}
 
 	if err := yaml.Unmarshal(file, &qua); err != nil {
+		logrus.Error(err)
 		return nil, err
 	}
 
-	return qua, nil
+	switch qua.RawMode {
+	case "Keys4":
+	case "1":
+		qua.Mode = enums.GameModeKeys4
+		break
+	case "Keys7":
+	case "2":
+		qua.Mode = enums.GameModeKeys7
+		break
+	}
+
+	return &qua, nil
 }
