7171a812549d488dada85a8a324f26a181d0844f
Swan <me@swan.moe>
2024-06-14 17:59:10 -0400

    Add omitempty for user stats and client status

--- db/users.go
+++ db/users.go
@@ -44,9 +44,9 @@ type User struct {
 	ClanLeaveTime               int64             `gorm:"column:clan_leave_time" json:"-"`
 	ClanLeaveTimeJSON           time.Time         `gorm:"-:all" json:"clan_leave_time"`
 	ShadowBanned                bool              `gorm:"column:shadow_banned" json:"-"`
-	ClientStatus                *UserClientStatus `gorm:"-:all" json:"client_status"`
-	StatsKeys4                  *UserStatsKeys4   `gorm:"foreignKey:UserId" json:"stats_keys4"`
-	StatsKeys7                  *UserStatsKeys7   `gorm:"foreignKey:UserId" json:"stats_keys7"`
+	ClientStatus                *UserClientStatus `gorm:"-:all" json:"client_status,omitempty"`
+	StatsKeys4                  *UserStatsKeys4   `gorm:"foreignKey:UserId" json:"stats_keys4,omitempty"`
+	StatsKeys7                  *UserStatsKeys7   `gorm:"foreignKey:UserId" json:"stats_keys7,omitempty"`
 }
 
 type UserClientStatus struct {
