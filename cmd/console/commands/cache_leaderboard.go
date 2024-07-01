3e20d80218926b24c2ea5af05dbf6c4f7dc8612b
Swan <me@swan.moe>
2024-07-01 16:46:27 -0400

    Add error handling to cache leaderboard command

--- cmd/console/commands/cache_leaderboard.go
+++ cmd/console/commands/cache_leaderboard.go
@@ -15,16 +15,19 @@ var CacheLeaderboardCmd = &cobra.Command{
 	Use:   "cache:leaderboard",
 	Short: "Populates the leaderboards in cache",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Clear previous cached leaderboards
 		err := deleteOldLeaderboards()
+
 		if err != nil {
+			logrus.Error(err)
 			return
 		}
 
 		logrus.Println("Populating leaderboards...")
 
 		err = populateLeaderboard()
+
 		if err != nil {
+			logrus.Error(err)
 			return
 		}
 
@@ -56,7 +59,9 @@ func populateLeaderboard() error {
 			break
 		}
 
-		processUsers(users)
+		if err := processUsers(users); err != nil {
+			return err
+		}
 
 		offset += batchSize
 	}
@@ -64,28 +69,44 @@ func populateLeaderboard() error {
 	return nil
 }
 
-func processUsers(users []*db.User) {
+func processUsers(users []*db.User) error {
 	for _, user := range users {
-		db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:leaderboard:%v", enums.GameModeKeys4), redis.Z{
-			Score:  user.StatsKeys4.OverallPerformanceRating,
-			Member: strconv.Itoa(user.Id),
-		})
-
-		db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:leaderboard:%v", enums.GameModeKeys7), redis.Z{
-			Score:  user.StatsKeys7.OverallPerformanceRating,
-			Member: strconv.Itoa(user.Id),
-		})
-
-		if user.Country != "XX" {
-			db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:country_leaderboard:%v:%v", user.Country, enums.GameModeKeys4), redis.Z{
+		err := db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:leaderboard:%v", enums.GameModeKeys4),
+			redis.Z{
 				Score:  user.StatsKeys4.OverallPerformanceRating,
 				Member: strconv.Itoa(user.Id),
-			})
+			}).Err()
 
-			db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:country_leaderboard:%v:%v", user.Country, enums.GameModeKeys7), redis.Z{
+		if err != nil {
+			return err
+		}
+
+		err = db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:leaderboard:%v", enums.GameModeKeys7),
+			redis.Z{
 				Score:  user.StatsKeys7.OverallPerformanceRating,
 				Member: strconv.Itoa(user.Id),
-			})
+			}).Err()
+
+		if user.Country != "XX" {
+			err = db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:country_leaderboard:%v:%v",
+				user.Country, enums.GameModeKeys4), redis.Z{
+				Score:  user.StatsKeys4.OverallPerformanceRating,
+				Member: strconv.Itoa(user.Id),
+			}).Err()
+
+			if err != nil {
+				return err
+			}
+
+			err = db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:country_leaderboard:%v:%v",
+				user.Country, enums.GameModeKeys7), redis.Z{
+				Score:  user.StatsKeys7.OverallPerformanceRating,
+				Member: strconv.Itoa(user.Id),
+			}).Err()
+
+			if err != nil {
+				return err
+			}
 		}
 
 		var userKeys4Hits = user.StatsKeys4.TotalMarvelous + user.StatsKeys4.TotalPerfect +
@@ -93,11 +114,18 @@ func processUsers(users []*db.User) {
 		var userKeys7Hits = user.StatsKeys7.TotalMarvelous + user.StatsKeys7.TotalPerfect +
 			user.StatsKeys7.TotalGreat + user.StatsKeys7.TotalGood + user.StatsKeys7.TotalOkay
 
-		db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:leaderboard:total_hits_global"), redis.Z{
-			Score:  float64(userKeys4Hits) + float64(userKeys7Hits),
-			Member: strconv.Itoa(user.Id),
-		})
+		err = db.Redis.ZAdd(db.RedisCtx, fmt.Sprintf("quaver:leaderboard:total_hits_global"),
+			redis.Z{
+				Score:  float64(userKeys4Hits) + float64(userKeys7Hits),
+				Member: strconv.Itoa(user.Id),
+			}).Err()
+
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
 
 func deleteOldLeaderboards() error {
