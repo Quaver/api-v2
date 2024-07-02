f0ca28503320acef6d9c831c77601597a34b4e7c
Swan <me@swan.moe>
2024-07-02 11:30:11 -0400

    Move elasticsearch mapset functions to different file

---
+++ db/elasticsearch_mapsets.go
@@ -0,0 +1,150 @@
+package db
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/Quaver/api2/tasks"
+	"github.com/sirupsen/logrus"
+	"io"
+	"strings"
+	"time"
+)
+
+// IndexElasticSearchMapset Indexes an individual mapset in elastic
+func IndexElasticSearchMapset(mapset Mapset) error {
+	if err := DeleteElasticSearchMapset(mapset.Id); err != nil {
+		return err
+	}
+
+	mapset.User = nil
+	data, _ := json.Marshal(&mapset)
+
+	resp, err := ElasticSearch.Create(elasticMapsetIndex,
+		fmt.Sprintf("%v", mapset.Id), bytes.NewReader(data))
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	return nil
+}
+
+// DeleteElasticSearchMapset Deletes an individual mapset in elastic
+func DeleteElasticSearchMapset(id int) error {
+	resp, err := ElasticSearch.Delete(elasticMapsetIndex, fmt.Sprintf("%v", id))
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	return nil
+}
+
+// SearchElasticMapsets Searches ElasticSearch for mapsets
+func SearchElasticMapsets() ([]*Mapset, error) {
+	query := `{ "size" : 50, "query": { "match_all": {} } }`
+
+	resp, err := ElasticSearch.Search(
+		ElasticSearch.Search.WithIndex(elasticMapsetIndex),
+		ElasticSearch.Search.WithBody(strings.NewReader(query)),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	data := struct {
+		Hits struct {
+			Hits []struct {
+				Source Mapset `json:"_source"`
+			} `json:"hits"`
+		} `json:"hits"`
+	}{}
+
+	if err := json.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+
+	var mapsets []*Mapset
+
+	for _, hit := range data.Hits.Hits {
+		mapsets = append(mapsets, &hit.Source)
+	}
+
+	return mapsets, nil
+}
+
+// IndexAllElasticSearchMapsets Indexes all mapsets in the DB in ElasticSearch
+func IndexAllElasticSearchMapsets(deletePrevious bool, workers int) error {
+	if deletePrevious {
+		if err := DeleteElasticIndices(elasticMapsetIndex); err != nil {
+			return err
+		}
+	}
+
+	pool := tasks.NewWorkerPool(workers)
+
+	// Takes in a mapset and returns a bool if it was indexed successfully.
+	pool.Start(func(input ...interface{}) (interface{}, error) {
+		mapset := input[0].(*Mapset)
+
+		attempts := 0
+		const maxAttempts int = 10
+
+		for attempts < maxAttempts {
+			err := IndexElasticSearchMapset(*mapset)
+
+			if err != nil {
+				attempts++
+
+				logrus.Errorf("Failed to index mapset #%v. Retrying in 10 seconds...", mapset.Id)
+				time.Sleep(time.Second * 10)
+				continue
+			}
+
+			return true, nil
+		}
+
+		return false, errors.New(fmt.Sprintf("too many failed attempts to index: mapset #%v", mapset.Id))
+	})
+
+	mapsets, err := GetAllMapsets()
+
+	if err != nil {
+		return err
+	}
+
+	// Handle results in goroutine before adding tasks to prevent hanging
+	go func() {
+		for i := 0; i < len(mapsets); i++ {
+			result := pool.GetResult()
+			mapset := result.Input.(*Mapset)
+
+			if result.Error != nil {
+				logrus.Errorf("Error indexing mapset #%v: %v", mapset.Id, result.Error)
+				continue
+			}
+
+			logrus.Infof("Successfully indexed mapset #%v", mapset.Id)
+		}
+	}()
+
+	// Put all mapsets into the task queue
+	for _, mapset := range mapsets {
+		pool.AddTask(mapset)
+	}
+
+	return nil
+}
