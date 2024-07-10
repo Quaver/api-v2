2a21db438731d7b65abf3a99638d0aa34ebbc82c
Swan <me@swan.moe>
2024-07-09 20:49:11 -0400

    Add endpoint to get donator prices

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -172,6 +172,7 @@ func initializeRoutes(engine *gin.Engine) {
 
 	// Orders
 	engine.GET("/v2/orders", middleware.RequireAuth, handlers.CreateHandler(handlers.GetUserOrders))
+	engine.GET("/v2/orders/donations/prices", handlers.CreateHandler(handlers.GetDonatorPrices))
 	engine.POST("/v2/orders/checkout", middleware.RequireAuth, handlers.CreateHandler(handlers.CreateOrderCheckoutSession))
 
 	// Orders Steam
