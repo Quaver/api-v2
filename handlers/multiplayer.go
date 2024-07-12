cd5dedfe72d81d13452c0566b514232ec5f5b3a7
Swan <me@swan.moe>
2024-07-12 11:19:27 -0400

    Return 404 for not found multiplayer game

--- handlers/multiplayer.go
+++ handlers/multiplayer.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"github.com/Quaver/api2/db"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 	"strconv"
 )
@@ -37,8 +38,12 @@ func GetMultiplayerGame(c *gin.Context) *APIError {
 
 	game, err := db.GetMultiplayerGame(id)
 
-	if err != nil {
+	if err != nil && err != gorm.ErrRecordNotFound {
 		return APIErrorServerError("Error retrieving multiplayer game", err)
+	}
+
+	if game == nil {
+		return APIErrorNotFound("Multiplayer Game")
 	}
 
 	c.JSON(http.StatusOK, gin.H{"game": game})
