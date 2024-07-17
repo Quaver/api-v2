207765b95de1dc279892a37455039f8a4f506488
Swan <me@swan.moe>
2024-07-17 11:33:40 -0400

    Add migration for user_notifications

---
+++ cmd/database/migrations/7_user_notifications.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE user_notifications
+(
+    id          INT     AUTO_INCREMENT,
+    sender_id   INT     NOT NULL,
+    receiver_id INT     NOT NULL,
+    type        TINYINT NOT NULL,
+    data        TEXT    NOT NULL,
+    read_at     BIGINT  NOT NULL,
+    timestamp   BIGINT  NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY `UNIQUE` (`id`)
+);
