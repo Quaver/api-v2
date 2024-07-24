9b2d3622e90f16f75398de07a853ecfa52ceb16a
Swan <me@swan.moe>
2024-07-24 14:18:43 -0400

    Add sort_order to migration

--- cmd/database/migrations/9_pinned_scores.up.sql
+++ cmd/database/migrations/9_pinned_scores.up.sql
@@ -1,12 +1,13 @@
 CREATE TABLE pinned_scores
 (
-    user_id  INT NOT NULL,
-    game_mode TINYINT NOT NULL,
-    score_id INT NOT NULL
+    user_id     INT     NOT NULL,
+    game_mode   TINYINT NOT NULL,
+    score_id    INT     NOT NULL,
+    sort_order  INT     NOT NULL
 );
 
 CREATE INDEX pinned_scores_user_id_game_mode_index
-    ON pinned_scores (user_id, game_mode);
+    ON pinned_scores (user_id, game_mode, sort_order);
 
 CREATE UNIQUE INDEX pinned_scores_user_id_score_id_uindex
     ON pinned_scores (user_id, game_mode, score_id);
