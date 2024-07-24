6310dc924b7486eee1a2eb78cdd1e44728042a85
Swan <me@swan.moe>
2024-07-24 11:00:22 -0400

    Add migrations for pinned scores

---
+++ cmd/database/migrations/9_pinned_scores.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE pinned_scores
+(
+    user_id  INT NOT NULL,
+    game_mode TINYINT NOT NULL,
+    score_id INT NOT NULL
+);
+
+CREATE INDEX pinned_scores_user_id_game_mode_index
+    ON pinned_scores (user_id, game_mode);
+
+CREATE UNIQUE INDEX pinned_scores_user_id_score_id_uindex
+    ON pinned_scores (user_id, game_mode, score_id);
