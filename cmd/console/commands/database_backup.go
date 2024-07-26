3ef411428ffcf8fd93234e256525afca38cde293
Swan <me@swan.moe>
2024-07-26 09:58:00 -0400

    Compress backup file into zip

--- cmd/console/commands/database_backup.go
+++ cmd/console/commands/database_backup.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"archive/zip"
 	"bytes"
 	"fmt"
 	"github.com/Azure/azure-storage-blob-go/azblob"
@@ -9,6 +10,7 @@ import (
 	"github.com/Quaver/api2/webhooks"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -24,14 +26,18 @@ var DatabaseBackupCmd = &cobra.Command{
 	Use:   "backup:database",
 	Short: "Backs up the database and uploads to azure",
 	Run: func(cmd *cobra.Command, args []string) {
+		currentTime := time.Now()
+		backupDir := fmt.Sprintf("%v/backups", config.Instance.Cache.DataDirectory)
+		sqlPath, _ := filepath.Abs(fmt.Sprintf("%v/backup.sql", backupDir))
+		zipPath, _ := filepath.Abs(fmt.Sprintf("%v/backup.sql.zip", backupDir))
+		azureFileName := fmt.Sprintf("%d-%d-%d-time-%d-%d.zip", currentTime.Year(),
+			currentTime.Month(), currentTime.Day(), currentTime.Hour(), currentTime.Minute())
+
 		if err := deletePreviousBackups(); err != nil {
 			logrus.Error(err)
 			_ = webhooks.SendBackupWebhook(false, err)
 			return
 		}
-
-		backupDir := fmt.Sprintf("%v/backups", config.Instance.Cache.DataDirectory)
-		path, _ := filepath.Abs(fmt.Sprintf("%v/backup.sql", backupDir))
 
 		if err := os.MkdirAll(backupDir, os.ModePerm); err != nil {
 			logrus.Error("[Database Backup] Error creating backup directory", err)
@@ -39,17 +45,27 @@ var DatabaseBackupCmd = &cobra.Command{
 			return
 		}
 
-		if err := os.Remove(path); err != nil {
+		if err := os.Remove(sqlPath); err != nil {
 			logrus.Error("[Database Backup] Error deleting existing backup")
 		}
 
-		if err := dumpDatabase(path); err != nil {
+		if err := os.Remove(zipPath); err != nil {
+			logrus.Error("[Database Backup] Error deleting existing backup")
+		}
+
+		if err := dumpDatabase(sqlPath); err != nil {
 			logrus.Error("[Database Backup] Error dumping database: ", err)
 			_ = webhooks.SendBackupWebhook(false, err)
 			return
 		}
 
-		if err := uploadToAzure(path); err != nil {
+		if err := zipBackup(sqlPath, zipPath); err != nil {
+			logrus.Error("[Database Backup] Error zipping database: ", err)
+			_ = webhooks.SendBackupWebhook(false, err)
+			return
+		}
+
+		if err := uploadToAzure(zipPath, azureFileName); err != nil {
 			logrus.Error("[Database Backup] Error uploading database backup", err)
 			_ = webhooks.SendBackupWebhook(false, err)
 			return
@@ -112,12 +128,44 @@ func dumpDatabase(path string) error {
 	return nil
 }
 
-func uploadToAzure(path string) error {
+func zipBackup(inputPath string, outputPath string) error {
+	logrus.Info("[Database Backup] Zipping backup file...")
+
+	inputFile, err := os.Open(inputPath)
+
+	if err != nil {
+		return fmt.Errorf("failed to open input file: %w", err)
+	}
+	defer inputFile.Close()
+
+	zipFile, err := os.Create(outputPath)
+
+	if err != nil {
+		return fmt.Errorf("failed to create zip file: %w", err)
+	}
+	defer zipFile.Close()
+
+	// Create a new zip writer
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	zipEntry, err := zipWriter.Create("backup.sql")
+
+	if err != nil {
+		return fmt.Errorf("failed to create zip entry: %w", err)
+	}
+
+	if _, err = io.Copy(zipEntry, inputFile); err != nil {
+		return fmt.Errorf("failed to copy file content into zip entry: %w", err)
+
+	}
+
+	logrus.Info("[Database Backup] Successfully zipped backup file")
+	return nil
+}
+
+func uploadToAzure(path string, fileName string) error {
 	logrus.Info("[Database Backup] Uploading to azure...:")
-
-	currentTime := time.Now()
-
-	fileName := fmt.Sprintf("%d-%d-%d-time-%d-%d.sql", currentTime.Year(), currentTime.Month(), currentTime.Day(), currentTime.Hour(), currentTime.Minute())
 
 	err := azure.Client.UploadFileFromDisk(databaseBackupContainer, fileName, path, azblob.AccessTierHot)
 
