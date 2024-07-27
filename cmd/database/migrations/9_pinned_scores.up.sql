1bef61566ee274363ab502d36b1e9de2b80bd087
Swan <me@swan.moe>
2024-07-27 09:51:41 -0400

    Allow multi statement queries in migrations

--- cmd/database/migrations/9_pinned_scores.up.sql
+++ cmd/database/migrations/9_pinned_scores.up.sql
@@ -1,4 +1,6 @@
-CREATE TABLE pinned_scores
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS pinned_scores
 (
     user_id     INT     NOT NULL,
     game_mode   TINYINT NOT NULL,
@@ -11,3 +13,6 @@ CREATE INDEX pinned_scores_user_id_game_mode_index
 
 CREATE UNIQUE INDEX pinned_scores_user_id_score_id_uindex
     ON pinned_scores (user_id, game_mode, score_id);
+
+COMMIT;
+
