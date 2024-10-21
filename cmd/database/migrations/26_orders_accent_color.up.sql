7b7934cacb8fc26063e0d43d806cb6ce23c32e29
Swan <me@swan.moe>
2024-10-21 14:36:30 -0400

    Allow accent color to be gifted

--- cmd/database/migrations/26_orders_accent_color.up.sql
+++ cmd/database/migrations/26_orders_accent_color.up.sql
@@ -3,7 +3,7 @@ BEGIN;
 INSERT INTO order_items
 (id, stripe_price_id, category, name, price_steam, price_stripe, max_qty_allowed, donator_bundle_item, in_stock, can_gift, visible, badge_id)
 VALUES
-    (3, '', 3, 'User Profile Accent Color', 649, 499, 1, 0, 1, 0, 1, null);
+    (3, '', 3, 'User Profile Accent Color', 649, 499, 1, 0, 1, 1, 1, null);
 
 ALTER TABLE users
     ADD accent_color_customizable TINYINT DEFAULT 0 NOT NULL;
