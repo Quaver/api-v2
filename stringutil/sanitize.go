fd76b9995e7ef8fdbf6d5d0e48a442d5b8532a93
AiAe <me@aiae.dev>
2024-10-26 23:06:01 +0300

    Allow br tag

--- stringutil/sanitize.go
+++ stringutil/sanitize.go
@@ -5,7 +5,7 @@ import "github.com/microcosm-cc/bluemonday"
 func SanitizeHTML(html string) string {
 	p := bluemonday.NewPolicy()
 
-	p.AllowElements("p", "b", "strong", "i", "em", "u", "ul", "ol", "li", "s", "a", "img", "table", "tr", "td", "center")
+	p.AllowElements("p", "b", "strong", "i", "em", "u", "ul", "ol", "li", "s", "a", "img", "table", "tr", "td", "center", "br")
 	p.AllowAttrs("style").OnElements("p")
 	p.AllowAttrs("href").OnElements("a")
 	p.AllowAttrs("src", "alt", "width", "height", "style").OnElements("img")
