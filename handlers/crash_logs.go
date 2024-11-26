824739b1cf1eb5dc4a117cfd1f7f861a3518ce9b
Swan <me@swan.moe>
2024-11-26 09:01:38 -0500

    Add crash log webhook

--- handlers/crash_logs.go
+++ handlers/crash_logs.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
-	"github.com/Quaver/api2/db"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
+
+	"github.com/Quaver/api2/db"
+	"github.com/Quaver/api2/webhooks"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 // AddCrashLog Adds a new crash log into the database
@@ -34,6 +37,10 @@ func AddCrashLog(c *gin.Context) *APIError {
 
 	if err := db.InsertCrashLog(crashLog); err != nil {
 		return APIErrorServerError("Error inserting crash log", err)
+	}
+
+	if err := webhooks.SendCrashLogWebhook(user, crashLog); err != nil {
+		logrus.Error("Error sending crash log webhook: ", err)
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Your crash log has been successfully submitted."})
