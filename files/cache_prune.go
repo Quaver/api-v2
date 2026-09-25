d6c6881c80f6eb7ef0dd636bec5fe79463d75e0e
AiAe <ayyayyedev@gmail.com>
2026-09-25 09:42:58 +0300

    Simplify the loop further

--- files/cache_prune.go
+++ files/cache_prune.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/Quaver/api2/config"
@@ -24,32 +22,17 @@ func PruneCache(deleteFiles bool) (CachePruneResult, error) {
 	var result CachePruneResult
 	root := config.Instance.Cache.DataDirectory
 	cutoff := time.Now().Add(-cacheRetention)
-	cacheDirectories := []struct {
-		name      string
-		extension string
-	}{
-		{"maps", ".qua"},
-		{"mapsets", ".qp"},
-		{"replays", ".qr"},
-	}
 
-	for _, cacheDirectory := range cacheDirectories {
-		directory := filepath.Join(root, cacheDirectory.name)
+	for _, name := range []string{"maps", "mapsets", "replays"} {
+		directory := filepath.Join(root, name)
 		entries, err := os.ReadDir(directory)
 		if err != nil {
 			return result, fmt.Errorf("read cache directory %s: %w", directory, err)
 		}
 
 		for _, entry := range entries {
-			if !isCacheFile(entry.Name(), cacheDirectory.extension) {
-				continue
-			}
-
 			path := filepath.Join(directory, entry.Name())
 			info, err := entry.Info()
-			if errors.Is(err, os.ErrNotExist) {
-				continue
-			}
 			if err != nil {
 				return result, fmt.Errorf("inspect cache file %s: %w", path, err)
 			}
@@ -58,21 +41,7 @@ func PruneCache(deleteFiles bool) (CachePruneResult, error) {
 			}
 
 			if deleteFiles {
-				// Skip files that were used or replaced while scanning.
-				currentInfo, err := os.Lstat(path)
-				if errors.Is(err, os.ErrNotExist) {
-					continue
-				}
-				if err != nil {
-					return result, fmt.Errorf("inspect cache file %s: %w", path, err)
-				}
-				if !os.SameFile(info, currentInfo) || !currentInfo.ModTime().Before(cutoff) {
-					continue
-				}
 				if err := os.Remove(path); err != nil {
-					if errors.Is(err, os.ErrNotExist) {
-						continue
-					}
 					return result, fmt.Errorf("remove cache file %s: %w", path, err)
 				}
 			}
@@ -83,13 +52,4 @@ func PruneCache(deleteFiles bool) (CachePruneResult, error) {
 	}
 
 	return result, nil
-}
-
-func isCacheFile(name string, extension string) bool {
-	if !strings.HasSuffix(name, extension) {
-		return false
-	}
-
-	id, err := strconv.Atoi(strings.TrimSuffix(name, extension))
-	return err == nil && id > 0
 }
